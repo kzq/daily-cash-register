4df3cca6a4e363f5996c5ae0d9a094c711ce9ac7
Kamran Zafar <kami_ravian@yahoo.com>
2017-11-01 16:05:05 +0000

    Add version.rb

---
+++ config/initializers/version.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module DailyCashRegister
+
+  def self.version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 0
+    MINOR = 1
+    PATCH  = 0
+
+    STRING = [MAJOR, MINOR, PATCH].compact.join(".")
+  end
+end
