4d28ed142f514b08119583770b22419ffa6f0edf
Kamran Zafar <kami_ravian@yahoo.com>
2017-11-13 17:00:36 +0000

    setup secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Read encrypted secrets
+  config.read_encrypted_secrets = true
+
 end
