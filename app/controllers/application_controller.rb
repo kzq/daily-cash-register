cdb48a55c2103523291df3f00a161df178cfa174
Kamran Zafar <kami_ravian@yahoo.com>
2017-11-15 10:06:56 +0000

    Add guest layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  # Require authentication for all requests. Add
+  # skip_before_action :authenticate_user! to controllers that should not
+  # require authentication.
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  layout :set_layout
+
+  private
+
+  # Choose from 2 types of layouts: guest (not logged-in), user
+  def set_layout
+    user_signed_in? ? 'user' : 'guest'
+  end
+
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
    def new
+     store_return_to
      super
    end
 
