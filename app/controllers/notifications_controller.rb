8618b9b77c759bc221754734d62ef5247936715e
Kamran Zafar <kami_ravian@yahoo.com>
2017-11-23 10:20:15 +0000

    Add notification resource

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,79 @@
+class NotificationsController < ApplicationController
+  # GET /notifications
+  # GET /notifications.json
+  def index
+    @notifications = Notification.order('created_at desc').limit(20)
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @notifications }
+    end
+  end
+
+  # GET /notifications/new
+  # GET /notifications/new.json
+  def new
+    @notification = Notification.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @notification }
+    end
+  end
+
+  # GET /notifications/1/edit
+  def edit
+    @notification = Notification.find(params[:id])
+  end
+
+  # POST /notifications
+  # POST /notifications.json
+  def create
+    @notification = Notification.new(notification_params)
+
+    respond_to do |format|
+      if @notification.save
+        format.html { redirect_to @notification, notice: 'notification was successfully created.' }
+        format.json { render json: @notification, status: :created, location: @notification }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @notification.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /notifications/1
+  # PUT /notifications/1.json
+  def update
+    @notification = Notification.find(params[:id])
+
+    respond_to do |format|
+      if @notification.update_attributes(notification_params)
+        format.html { redirect_to @notification, notice: 'notification was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @notification.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /notifications/1
+  # DELETE /notifications/1.json
+  def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+
+    respond_to do |format|
+      format.html { redirect_to notifications_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def notification_params
+    params.permit(notifications)
+  end
+
+
+end
