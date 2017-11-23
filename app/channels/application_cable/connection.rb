d01ee1683a332080d8d31796e1d010d343198c25
Kamran Zafar <kami_ravian@yahoo.com>
2017-11-23 14:21:08 +0000

    verify users before connecting to websockets

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    # prevent users from connecting to the websockets
+    # without a user account logged in
+    def connect
+      self.current_user = find_verfied_user
+    end
+
+    protected
+
+    def find_verfied_user
+      if current_user = env['warden'].user
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -5,5 +5,6 @@ class NotificationsChannel < ApplicationCable::Channel
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
