18aff9b7fb35b8cb73c286be61c4eda2b231058b
Kamran Zafar <kami_ravian@yahoo.com>
2018-01-11 14:16:28 +0000

    test suit for weekly_sale

---
+++ spec/factories/weekly_sales.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :weekly_sale do
+    sale  30000
+    rent  9730
+    cost  14890
+    gross_profit 6000
+    net_profit 5500
+  end
+end
--- spec/models/weekly_sale_spec.rb
+++ spec/models/weekly_sale_spec.rb
@@ -11,5 +11,34 @@ RSpec.describe WeeklySale, type: :model do
   end
 
   context "with invalid parameters" do
+    it "is invalid without sale" do
+      expect(build(:weekly_sale, sale: nil)).not_to be_valid
+    end
+
+    describe "rent validations" do
+      it "is invalid without rent" do
+        weekly_sale = build(:weekly_sale, rent: nil)
+        weekly_sale.valid?
+        expect(weekly_sale.errors[:rent]).to include("can't be blank")
+      end
+
+      it "is invalid if not a number" do
+        weekly_sale = build(:weekly_sale, rent: 'abc')
+        weekly_sale.valid?
+        expect(weekly_sale.errors[:rent]).to include("is not a number")
+      end
+    end
+
+    it "is invalid without cost" do
+     expect(build(:weekly_sale, cost: nil)).not_to be_valid
+    end
+
+    it "is invalid without gross_profit" do
+      expect(build(:weekly_sale, gross_profit: nil)).not_to be_valid
+    end
+
+    it "is invalid without net_profit" do
+      expect(build(:weekly_sale, net_profit: nil)).not_to be_valid
+    end
   end
 end
