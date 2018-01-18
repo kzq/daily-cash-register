b560f519a520df0c1fa1f7e137075cbec8badf0b
Kamran Zafar <kami_ravian@yahoo.com>
2018-01-18 16:12:34 +0000

    configure rails generators

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,19 @@ module DailyCashRegister
     config.generators.system_tests = nil
 
     MiniMagick.configure do |config|
-  config.validate_on_create = false
-  config.validate_on_write = false
-end
+      config.validate_on_create = false
+      config.validate_on_write = false
+    end
+
+   config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
