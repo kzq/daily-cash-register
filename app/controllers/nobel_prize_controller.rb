f19d312a86f4b4582ea89b16d096a3d8059f08dc
Kamran Zafar <kami_ravian@yahoo.com>
2018-01-26 12:47:18 +0000

    nobel prize controller specs

--- app/controllers/nobel_prize_controller.rb
+++ app/controllers/nobel_prize_controller.rb
@@ -1,6 +1,7 @@
 class NobelPrizeController < ApplicationController
   def index
-    @dashboard_cards = DashboardCard.create([{ value: "15", label: "Math"}, { value: "12", label: "Computing"},{ value: "12", label: "Physics"}])
+    data = [{ value: "15", label: "Math"}, { value: "12", label: "Computing"},{ value: "12", label: "Physics"}]
+    @dashboard_cards = DashboardCard.create(data)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @dashboard_cards, status: :ok }
--- spec/controllers/nobel_prize_controller_spec.rb
+++ spec/controllers/nobel_prize_controller_spec.rb
@@ -9,13 +9,24 @@ RSpec.describe NobelPrizeController, type: :controller do
     login_user
 
     describe "GET #index" do
-      before :each do
-        get :index
+      before :each do |example|
+        unless example.metadata[:skip_before]
+          get :index
+        end
       end
 
       include_examples "request response examples", :index
 
-      it "populates nobel_prizes to @nodel_prizes"
+      it "populates array of all dashboard cards", :skip_before do
+        data = [{ value: "15", label: "Math"}, { value: "12", label: "Computing"},{ value: "12", label: "Physics"}]
+        dashboard_card = class_double("DashboardCard").as_stubbed_const
+        math_card = double("DashboardCard", value: "15", label: "Math")
+        computing_card = double("DashboardCard", value: "12", label: "Computing")
+        dashboard_cards = [ math_card, computing_card ]
+        expect(dashboard_card).to receive(:create).with(data).and_return(dashboard_cards)
+        get :index
+        expect(assigns(:dashboard_cards)).to match_array(dashboard_cards)
+      end
     end
   end
 end
