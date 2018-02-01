827d93aea12e64d9094b8ed38125d3ab8dc4a993
Kamran Zafar <kami_ravian@yahoo.com>
2018-02-01 15:33:24 +0000

    Load nobel prizes sample data
    sample json data given in file

--- spec/services/nobel_prize_data_collector_spec.rb
+++ spec/services/nobel_prize_data_collector_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe NobelPrizeDataCollector do
   let(:data_collector) { described_class.new }
-  let(:response) { { prize: "prize" }.to_json }
+  let(:response) { file_fixture("nobel_prizes.json").read }
 
   before :each do
     stub_request(:get, "http://api.nobelprize.org/v1/prize.json").to_return(body: response, status: 200)
@@ -14,8 +14,9 @@ RSpec.describe NobelPrizeDataCollector do
     it "fetches data from the external feed" do
       data = data_collector.collect
       expect(data).to be_an_instance_of(Hash)
-      expect(data.dig("prize")).to eq("prize")
       expect(data).to eq(JSON.parse(response))
+      expect(data.dig("prizes")).to be_an_instance_of(Array)
+      expect(data.dig("prizes",1,"year")).to eq("2017")
     end
   end
 end
